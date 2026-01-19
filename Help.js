477ee8a6f16b42be3bbb2d88bc5e205b592ae7a0
Indopeople1 <hitammlah@gmail.com>
2026-01-19 19:50:37 +0700

    Create Help.js
    Kode testing

---
+++ Help.js
@@ -0,0 +1,9 @@
+const budi = false;
+
+if (budi === true) {
+  console.log("aku menjadi kapal laut");
+} if else (budi === "hytam") {
+  console.log("budi main rodok-rodok");
+} else {
+  console.log("budi transgender");
+}
